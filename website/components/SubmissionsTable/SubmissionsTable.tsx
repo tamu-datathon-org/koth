7d5ecd49192b9c6688a227dad33cfdaa84aa527d
phulsechinmay <phulsechinmay@gmail.com>
2020-10-16 00:33:02 -0500

    feat: Added submission status to submission table on problems page

--- website/components/SubmissionsTable/SubmissionsTable.tsx
+++ website/components/SubmissionsTable/SubmissionsTable.tsx
@@ -2,9 +2,24 @@ import * as dayjs from "dayjs";
 import React from "react";
 import { Button, Table } from "react-bootstrap";
 import { Submission } from "../../libs/submissions-api";
-
+import styles from "./submissions-table.module.scss";
 const relativeTime = require("dayjs/plugin/relativeTime");
 dayjs.extend(relativeTime);
+
+const statusStyleMap = (status: string): any => {
+  switch (status) {
+    case "SUBMITTED":
+      return styles.submitted;
+    case "IN_PROGRESS":
+      return styles.inProgress;
+    case "SUCCESS":
+      return styles.success;
+    case "ERROR":
+      return styles.error;
+    default:
+      return styles.defaultStatus;
+  }
+};
 
 export interface SubmissionsTableProps {
   submissions: Submission[];
@@ -23,6 +38,7 @@ export const SubmissionsTable: React.FC<SubmissionsTableProps> = ({
         <tr>
           <th>#</th>
           <th>Submission Time</th>
+          <th>Status</th>
           <th>Score</th>
         </tr>
       </thead>
@@ -39,6 +55,7 @@ export const SubmissionsTable: React.FC<SubmissionsTableProps> = ({
               </Button>
             </td>
             <td>{dayjs.unix(val.creationTimestamp).fromNow()}</td>
+            <td className={statusStyleMap(val.status)}>{val.status}</td>
             <td>{val.score}</td>
           </tr>
         ))}
