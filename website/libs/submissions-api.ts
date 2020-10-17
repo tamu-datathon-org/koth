0ea19920b7994c79008da71aa1063dccdae75ae9
phulsechinmay <phulsechinmay@gmail.com>
2020-10-17 04:59:52 -0500

    feat: Changed AWS env variable name to have KOTH_ prefix

--- website/libs/submissions-api.ts
+++ website/libs/submissions-api.ts
@@ -4,8 +4,8 @@ import { S3 } from "aws-sdk";
 const s3 = new S3({
   region: "us-east-1",
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+    accessKeyId: process.env.KOTH_AWS_ACCESS_KEY_ID!,
+    secretAccessKey: process.env.KOTH_AWS_SECRET_ACCESS_KEY!,
   },
 });
 
