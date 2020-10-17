e198dbe29636c5c1a0a057958ec8b937c9d29bb1
George Thayamkery <george.thayamkery@gmail.com>
2020-10-16 22:57:23 -0500

    S3 bucket (#5)
    * s3 stuff
    
    * fixes

--- website/next.config.js
+++ website/next.config.js
@@ -3,6 +3,9 @@ module.exports = {
     env: {
         GATEKEEPER_URL: "https://tamudatathon.com/auth",
         WORKER_SPAWNER_URL: "http://example.com/koth/admin",
-        WORKER_SPAWNER_KEY: "secretKey"
+        WORKER_SPAWNER_KEY: "secretKey",
+        GATEKEEPER_URL: "https://tamudatathon.com/auth",
+        DB_PROBLEMS_COLLECTION: "problems",
+        DB_SUBMISSIONS_COLLECTION: "submissions"
     }
 }
