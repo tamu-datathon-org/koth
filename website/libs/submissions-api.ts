d949dbca6d7b04ca8e76be4d69cbc6653b99f3c2
phulsechinmay <phulsechinmay@gmail.com>
2020-10-16 22:50:52 -0500

    feat: Created basic structure for submissions paage

--- website/libs/submissions-api.ts
+++ website/libs/submissions-api.ts
@@ -4,11 +4,13 @@ import { firebaseBucket } from "./firebase";
 const SUBMISSIONS_COLLECTION =
     process.env.DB_SUBMISSIONS_COLLECTION || "submissions";
 
+export type SubmissionStatus = "SUBMITTED" | "IN_PROGRESS" | "SUCCESS" | "ERROR"
+
 export interface Submission {
     id: string;
     userAuthId: string;
     problemId: string;
-    status: string;
+    status: SubmissionStatus;
     score: number;
     creationTimestamp: number;
     data?: any;
--- website/pages/submissions/[id].tsx
+++ website/pages/submissions/[id].tsx
@@ -1,9 +1,20 @@
 import axios from "axios";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
-import { Button } from "react-bootstrap";
+import { Terminal } from "xterm";
+import { Button, Col, Container, Row, Toast } from "react-bootstrap";
 import { LoadingSpinner } from "../../components/LoadingSpinner";
+import { statusStyleMap } from "../../components/SubmissionsTable";
 import { Submission } from "../../libs/submissions-api";
+import styles from "./[id].module.scss";
+
+const TEST_OUTPUT = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+
+Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+
+uis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+`;
 
 interface SubmissionPageData {
   submission: Submission;
@@ -13,21 +24,35 @@ const SubmissionPage = () => {
   const router = useRouter();
   const [data, setData] = useState<SubmissionPageData | null>(null);
   const [error, setError] = useState<boolean>(false);
+  const [terminal, setTerminal] = useState<Terminal | null>(null);
+  const [showToast, setShowToast] = useState(false);
   const submissionId = router.query.id as string;
 
   const fetchData = async () => {
     if (!submissionId) return;
     try {
       const resp = await axios.get(`/koth/api/submissions/${submissionId}`);
-      setData(resp.data as SubmissionPageData);
+      const data = resp.data as SubmissionPageData;
+
+      // const terminalOutput = data.submission.data?.output || TEST_OUTPUT;
+      // terminal?.reset();
+      // terminal?.write(terminalOutput);
+
+      setData(data);
     } catch (e) {
       setError(true);
     }
   };
-
   useEffect(() => {
     fetchData();
   }, [submissionId]);
+
+  // Initialize terminal.
+  // useEffect(() => {
+  //   const terminal = new Terminal();
+  //   terminal.open(document.getElementById("terminalOutput")!);
+  //   setTerminal(terminal);
+  // }, []);
 
   if (error) {
     return <h1>Something went wrong!</h1>;
@@ -36,40 +61,63 @@ const SubmissionPage = () => {
     return LoadingSpinner;
   }
 
-  const handleFileSubmit = async (file: File) => {
-    try {
-      const urlResponse = await axios.get("/koth/api/submission-signed-url");
-      const signedUrl = urlResponse.data.url[0];
-      var formData = new FormData();
-      formData.append("file", file);
-      axios.put(signedUrl, formData, {
-        headers: {
-          "Access-Control-Allow-Origin": "*",
-          "Content-Type": "multipart/form-data",
-        },
-      });
-    } catch (e) {}
-  };
-
-  const handleSubmissionBtnClick = () => {
-    // Hack to get a file dialog box.
-    const input = document.createElement("input");
-    input.type = "file";
-
-    input.onchange = (e: any) => {
-      handleFileSubmit(e.target.files[0]);
-    };
-    input.click();
-  };
-
   const { submission } = data;
   return (
-    <>
-      <h1>{submission.id}</h1>
-      <p>{submission.status}</p>
-      <p>{submission.creationTimestamp}</p>
-      <Button onClick={handleSubmissionBtnClick}>Select a file</Button>
-    </>
+    <Container fluid className={styles.submissionsPage}>
+      {/* Toast for copying submission link */}
+      <Toast
+        onClose={() => setShowToast(false)}
+        style={{
+          position: "absolute",
+          top: "70px",
+          right: "20px",
+        }}
+        show={showToast}
+        delay={2000}
+        autohide
+      >
+        <Toast.Header>
+          <small>Copied submission link to clipboard!</small>
+        </Toast.Header>
+        {/* <Toast.Body>See? Just like this.</Toast.Body> */}
+      </Toast>
+      <Row className="justify-content-center ">
+        <Col sm="auto" className={styles.submissionContainer}>
+          <Row className="mb-4 justify-content-between align-items-center">
+            <Button
+              variant="outline-primary"
+              onClick={() => {
+                navigator.clipboard.writeText(window.location.href);
+                setShowToast(true);
+              }}
+            >
+              Copy Submission Link
+            </Button>
+            <h5>
+              Status:{" "}
+              <span className={statusStyleMap(submission.status)}>
+                {submission.status}
+              </span>
+            </h5>
+          </Row>
+          <Row className={`mb-3`}>
+            <h1>Your Submission</h1>
+          </Row>
+          <Row className={`mb-3 justify-content-between`}>
+            <h5>Problem: {submission.problemId}</h5>
+            <h5>Score: {submission.score}</h5>
+          </Row>
+          <Row>
+            <h5>Output:</h5>
+          </Row>
+          <Row>
+            <pre className={`${styles.outputDiv}`} id="terminalOutput">
+              {submission.data?.output || TEST_OUTPUT}
+            </pre>
+          </Row>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
